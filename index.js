b5ecdbae486b9fc203f6be51b2dfd46e6de5253f
D-ng1277 <haidangbachkhoa@gmail.com>
2024-10-30 22:54:12 +0700

    added global error handler

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+console.log(process.env.PORT);
+console.log(decodeURIComponent(process.env.MONGO_URL));
 const database = require('./config/database.js');
 const routeAdmin = require('./api/routes/admin/index.route.js');
 const route = require('./api/routes/client/index.route.js');
 const Product = require('./api/models/product.model.js');
+const globalErrorHandler = require('./middleware/globalErrorHandler.js');
 
 database.connect();
 
@@ -16,3 +19,5 @@ route(app);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+app.use(globalErrorHandler);
