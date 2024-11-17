ddf9e9f42469df94af1ff24a842a7bfc6262476a
lap <luuchilap@gmail.com>
2024-11-17 22:38:37 +0700

    Cart

--- api/routes/client/cart.route.js
+++ api/routes/client/cart.route.js
@@ -5,4 +5,10 @@ const controller = require('../../controllers/client/cart.controller.js');
 
 router.get("/", controller.index);
 
+router.post("/:productId", controller.addPost);
+
+router.delete('/delete/:productId', controller.delete);
+
+router.patch('/update/:productId', controller.update);
+
 module.exports = router;
